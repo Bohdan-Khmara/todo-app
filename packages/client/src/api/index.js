dc112fa3e6786d5588c0cf71fbea822fe7aa0a69
User Name <artur.it2914@gmail.com>
2020-10-12 14:47:52 +0300

    Add api module exports

--- packages/client/src/api/index.js
+++ packages/client/src/api/index.js
@@ -5,13 +5,16 @@ const apiInstance = axios.create({
   baseURL: `http://localhost:${process.env.PORT ?? 5000}/api`,
 });
 
-const createTodo = data => apiInstance.post('/todos', data);
+export const createTodo = data => apiInstance.post('/todos', data);
 
-const getTodos = params =>
+export const getTodos = params =>
   apiInstance.get(`/todos?${queryString.stringify(params)}`);
 
-const getTodoById = id => apiInstance.get(`todos/${id}`);
+export const getTodoById = id => apiInstance.get(`todos/${id}`);
 
-const updateTodoById = (id, data) => apiInstance.patch(`todos/${id}`, data);
+export const updateTodoById = (id, data) =>
+  apiInstance.patch(`todos/${id}`, data);
 
-const removeTodoById = id => apiInstance.delete(`todos/${id}`);
+export const removeTodoById = id => apiInstance.delete(`todos/${id}`);
+
+export default apiInstance;
